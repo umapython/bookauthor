4dd89faa37e596abded8e1f197c57f8318f009a9
umapython <pdxuma@gmail.com>
2022-05-21 06:04:56 -0700

    Second commit -updated views for response,status error

--- bookproject/bookapp/views.py
+++ bookproject/bookapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from rest_framework import  serializers
+from rest_framework import  serializers,status
 from .serializers import RegisterSerializer,Book_detailSerializer,LoginSerializer
 from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.permissions import AllowAny
 # Create your views here.
